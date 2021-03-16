d52a233e134fa8b4a84b0145e8a84bc1dea7dae6
Dragos Dumitru Dijmarescu <80206286+dragos-dijmarescu@users.noreply.github.com>
2021-03-16 14:27:29 +0200

    Add files via upload

---
+++ Examples.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+//function declaration
+int func();
+
+int main() {
+
+   //function call
+   int i = func();
+   printf("%d",i);
+
+}
+
+//function definition
+int func()
+{
+   return 0;
+}
